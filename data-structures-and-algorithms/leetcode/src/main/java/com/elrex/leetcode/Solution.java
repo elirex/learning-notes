5d6ca35902fb9960ce5b92ed1ec728057a6ef6bf
elirex <sheng79225@gmail.com>
2020-10-28 22:06:02 +0800

    [leetcode] Problem 769

---
+++ data-structures-and-algorithms/leetcode/src/main/java/com/elrex/leetcode/Solution.java
@@ -0,0 +1,35 @@
+package com.elrex.leetcode;
+
+/**
+ * Problem: 769. Max Chunks To Make Sorted
+ * @see <a href="https://leetcode.com/problems/max-chunks-to-make-sorted/description/">link</a>
+ * Created by Wang, Sheng-Yuan (Elirex) on 2020/10/28.
+ */
+public class Solution {
+
+    public int maxChunksToSorted(int[] arr) {
+        if (arr == null || arr.length == 0) return 0;
+        int[] max = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            if (i == 0) max[i] = arr[i];
+            else max[i] = Math.max(max[i - 1], arr[i]);
+        }
+        int chunksCount = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (max[i] == i) chunksCount++;
+        }
+        return chunksCount;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] input = new int[] {4, 3, 2, 1, 0};
+        // Output = 1
+        System.out.println(solution.maxChunksToSorted(input));
+
+        input = new int[] {1, 0, 2, 3, 4};
+        // Output = 4
+        System.out.println(solution.maxChunksToSorted(input));
+    }
+
+}
